a321f2021078caba127ae6898a79a848d028154d
atka03 <151945345+atka03@users.noreply.github.com>
2025-05-16 16:32:26 +0200

    chore(q1 word_guess): inital commit with file structure

---
+++ q1_word_guess.py
@@ -0,0 +1,27 @@
+#Importering
+import random
+
+
+
+def selectRandomLine(file):
+    line = next(file)
+    for num, a_line in enumerate(file, 2):
+        if random.randrange(num):
+            continue
+        line = a_line
+    return line
+    
+def print_random_line(file):
+    print(selectRandomLine(file))
+
+def main():
+    """The main function that runs the application"""
+    
+    #Opening a file 
+    file = open("word_guess.txt", "r")
+    
+    print_random_line(file)
+    
+    file.close()
+
+main()    
