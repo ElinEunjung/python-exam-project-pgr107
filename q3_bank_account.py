fd479650d3773261f5cc6b5decd36fd7cfdf3b30
Ivan <ivan.kovals@hotmail.com>
2025-05-20 12:33:03 +0200

    feat(q3) add base functionality

---
+++ q3_bank_account.py
@@ -0,0 +1,95 @@
+# options = {
+#     1: "Open a new account",
+#     2: "Deposit money into your account",
+#     3: "Withdraw money from your account",
+#     4: "Add interests to your account",
+#     5: "Get the current balance of your account",
+#     6: "Quit"
+# }
+
+class Menu:
+    """Class for controlling flow of the program"""
+
+    def __init__(self):
+        self.options = [        # creates a list with pre-defined options
+            "1. Open a new account",
+            "2. Deposit money into your account",
+            "3. Withdraw money from your account",
+            "4. Add interests to your account",
+            "5. Get the current balance of your account",
+            "6. Quit"
+        ]
+
+    def add_option(self):
+        option = input("Write an option you would like to add: ")
+        # option = str(len(self.options)) + ". "  + option
+        self.options.append(option)
+        print(f"New option {option} added successfully.\n ")
+
+    def get_input(self):
+        self.print_menu()
+        user_input = input("Enter your choice with number: ")
+        if user_input in {"1", "2", "3", "4", "5", "6"}:
+            return int(user_input)
+        else:
+            return None
+
+    def print_menu(self):
+        for option in self.options:
+            print(option)
+
+
+class BankAccount:
+
+    def __init__(self, balance = 0.0, interest = 0.0):
+        self.balance = balance
+        self.interest = interest
+
+    def deposit(self):
+        amount = float(input("Enter amount to deposit: "))
+        self.balance += amount
+        print(f"Your balance was updated, now: {self.balance}")
+
+    def withdraw(self):
+        amount = float(input("Enter amount to deposit: "))
+        self.balance -= amount
+        print(f"Your balance was updated, now: {self.balance}")
+
+    def add_interests(self):
+        interest = float(input("Add interest rate, user numbers like 2.50: "))
+        self.interest = interest
+        print(f"Interest was added, after 1 year you may earn: {self.interest / 100 * self.balance}")
+
+    def get_balance(self):
+        return self.balance
+
+
+if __name__ == "__main__":
+    """Only runs if this script is the main file, was added 
+       to harmonize code with our common standards"""
+
+    bank_account_1 = BankAccount()
+    menu = Menu()
+    user_choice = menu.get_input()
+    # if user_choice is not None:
+    while user_choice != 6:
+        if user_choice is None:
+            print("Please enter a valid option.")
+        elif user_choice == 1:
+            print("Opened a new account")
+        elif user_choice == 2:
+            bank_account_1.deposit()
+        elif user_choice == 3:
+            bank_account_1.withdraw()
+        elif user_choice == 4:
+            bank_account_1.add_interests()
+        elif user_choice == 5:
+            print(bank_account_1.get_balance())
+        print()
+        user_choice = menu.get_input()
+    print("Exiting program")
+
+
+
+
+
