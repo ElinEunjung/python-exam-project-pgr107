3aa5ba65b47361bd0efdc27ff3373f362e4815a0
atka03 <151945345+atka03@users.noreply.github.com>
2025-05-23 17:36:31 +0200

    style(q1): fix grammar, removed unessecary parenthesis

--- q1_word_guess.py
+++ q1_word_guess.py
@@ -19,7 +19,7 @@ def display_question(file):
     # A array of letters not in the answer
     wrong_letters = []
     
-    # A intiger that representes the number of letters 
+    # An integer that represents the number of letters 
     n_letters = len(correct_answer) 
     
     # A intiger variable denoting the number of guesses that the user has left
@@ -28,9 +28,9 @@ def display_question(file):
     # The blank lines used as a plaseholder for the letters of the chosen word
     letter_display =  "_" * len(correct_answer)
     
-    # Loop to update the diplay as you guess letters 
+    # Loop to update the display as you guess letters 
     while n_guesses > 0:
-        print(f"The word has letters {n_letters} and you have {n_guesses} guesses left.\n")
+        print(f"The word has {n_letters} letters and you have {n_guesses} guesses left.\n")
         print(f"Your word: {letter_display}")
         
         user_input = input("Letter: ")
@@ -38,21 +38,21 @@ def display_question(file):
         
         is_valid_input = validate_user_input(user_input)
         
-        if(is_valid_input == True):
+        if(is_valid_input):
             # If black used to both check if you have already guessed a letter  
-            if(user_input in letter_display or user_input in wrong_letters):
+            if user_input in letter_display or user_input in wrong_letters :
                 print("You have alredy guessed this")
                 continue
             # elif used to that this logic is run if the if statement above is false 
-            elif(user_input in correct_answer):
-                print("Corret")
+            elif user_input in correct_answer:
+                print("Correct")
                 letter_display = update_display(correct_answer, user_input, letter_display)
                 n_guesses += 1
             else:
                 print("wrong")
                 wrong_letters.append(user_input)
 
-            if(letter_display == correct_answer):
+            if letter_display == correct_answer:
                 print_victory_message(correct_answer)
                 break
         
@@ -62,17 +62,17 @@ def display_question(file):
             print("Invalid input")
         
     
-    if(n_guesses == 0 and letter_display != correct_answer):
+    if n_guesses == 0 and letter_display != correct_answer:
         print(f"\nYou lost. The correct word was: {correct_answer}")
 
 
 def validate_user_input(user_input):
     valid_input = True
     # If blocks used for input validation
-    if(len(user_input) > 1):
+    if len(user_input) > 1:
         valid_input = False
           
-    if (not user_input.isalpha()):
+    if not user_input.isalpha():
         valid_input = False
         
     return valid_input
@@ -83,7 +83,7 @@ def update_display(correct_answer, user_input, letter_display):
     letter_display = list(letter_display)
     
     for i in range(len(correct_answer)):
-        if(correct_answer[i] == user_input):
+        if correct_answer[i] == user_input:
             letter_display[i] = user_input
         
     
